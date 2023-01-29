91e2b1ff1efeab0ac3bba505ad96ba296978fc51
Joey Van Lierop <joey.vanlierop@me.com>
2023-01-29 16:54:29 -0700

    Get everything working

--- client.c
+++ client.c
@@ -1,38 +1,117 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <netdb.h>
-#include <arpa/inet.h>
 #include <string.h>
+#include <time.h>
 #include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <time.h>
 
-int main() {
-    printf("This is the client\n");
+#define SERVER_IP "127.0.0.1"
+#define MAX_UCID_LEN 8
+#define MAX_DATETIME_LEN 25
+#define MAX_FILE_LEN 1000
 
-    struct sockaddr_in address;
-    memset(&address, 0, sizeof(address));
-    address.sin_family = AF_INET;
-    address.sin_port = htons(2000);
-    address.sin_addr.s_addr = inet_addr("127.0.0.1");
+int main(int argc, char *argv[])
+{
+	if (argc < 2)
+	{
+		printf("Usage: %s <port>\n", argv[0]);
+		exit(1);
+	}
 
-    int mysocket1;
-    mysocket1 = socket(AF_INET, SOCK_STREAM, 0);
-    if (mysocket1 == -1) {
-        printf("socket() call failed\n");
-    }
+	int port = atoi(argv[1]);
+	int client_socket, bytes_sent, bytes_received, passcode;
+	struct sockaddr_in server_address;
+	char ucid[MAX_UCID_LEN];
+	char datetime[MAX_DATETIME_LEN];
+	char file_contents[MAX_FILE_LEN];
+	FILE *file;
 
-    int status;
-    status = connect(mysocket1, (struct sockaddr *) &address, sizeof(struct sockaddr_in));
-    if (status == -1) {
-        printf("connect() call failed\n");
-    }
+	// Get user's UCID as input
+	printf("Enter your UCID: ");
+	scanf("%s", ucid);
 
-    int count;
-    char snd_message[100] = {"hello"};
-    count = send(mysocket1, snd_message, 5, 0);
-    if (count == -1) {
-        printf("send() call failed\n");
-    }
+	// Create a TCP connection with the server
+	client_socket = socket(AF_INET, SOCK_STREAM, 0);
+	if (client_socket < 0)
+	{
+		printf("Error creating client socket\n");
+		exit(EXIT_FAILURE);
+	}
+	memset(&server_address, 0, sizeof(server_address));
+	server_address.sin_family = AF_INET;
+	server_address.sin_addr.s_addr = inet_addr(SERVER_IP);
+	server_address.sin_port = htons(port);
+	if (connect(client_socket, (struct sockaddr *)&server_address, sizeof(server_address)) < 0)
+	{
+		printf("Error connecting to server\n");
+		exit(EXIT_FAILURE);
+	}
 
-    close(mysocket1);
+	// Send user's UCID to the server
+	bytes_sent = send(client_socket, ucid, MAX_UCID_LEN, 0);
+	if (bytes_sent < 0)
+	{
+		printf("Error sending user's UCID to server\n");
+		close(client_socket);
+		exit(EXIT_FAILURE);
+	}
+
+	// Read the current time sent by the server
+	bytes_received = recv(client_socket, datetime, MAX_DATETIME_LEN, 0);
+	if (bytes_received < 0)
+	{
+		printf("Error receiving current day and time from server\n");
+		close(client_socket);
+		exit(EXIT_FAILURE);
+	}
+	printf("Received current day and time: %s\n", datetime);
+
+	// Create the passcode
+	struct tm timeinfo;
+	memset(&timeinfo, 0, sizeof(struct tm));
+	strptime(datetime, "%F %T", &timeinfo);
+	passcode = timeinfo.tm_sec + atoi(ucid) % 10000;
+
+	// Send the passcode to the server
+	bytes_sent = send(client_socket, (const char *)&passcode, 4, 0);
+	if (bytes_sent < 0)
+	{
+		printf("Error sending passcode to server\n");
+		close(client_socket);
+		exit(EXIT_FAILURE);
+	}
+	printf("Sent passcode: %d\n", passcode);
+
+	// Read the file contents sent by the server
+	bytes_received = recv(client_socket, file_contents, MAX_FILE_LEN, 0);
+	if (bytes_received < 0)
+	{
+		printf("Error receiving file contents from server\n");
+		close(client_socket);
+		exit(EXIT_FAILURE);
+	}
+	printf("Received file contents: %s\n", file_contents);
+
+	// Save the file contents into a text file in the run directory
+	file = fopen("received_file.txt", "w");
+	if (file == NULL)
+	{
+		printf("Error opening file\n");
+		close(client_socket);
+		exit(EXIT_FAILURE);
+	}
+	if (fputs(file_contents, file) == EOF)
+	{
+		printf("Error writing to file\n");
+		fclose(file);
+		close(client_socket);
+		exit(EXIT_FAILURE);
+	}
+
+	// Close the file stream and socket
+	fclose(file);
+	close(client_socket);
+	return EXIT_SUCCESS;
 }
--- server.c
+++ server.c
@@ -1,50 +1,144 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <netdb.h>
 #include <string.h>
+#include <time.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
 
-int main() {
-    printf("This is the server\n");
+#define MAX_CLIENTS 5
+#define MAX_UCID_LEN 8
+#define MAX_DATETIME_LEN 25
+#define MAX_FILE_LEN 1000
 
-    struct sockaddr_in address;
-    memset(&address, 0, sizeof(address));
-    address.sin_family = AF_INET;
-    address.sin_port = htons(2000);
-    address.sin_addr.s_addr = htonl(INADDR_ANY);
+int main(int argc, char *argv[])
+{
+	if (argc < 2)
+	{
+		printf("Usage: %s <port>\n", argv[0]);
+		exit(1);
+	}
 
-    int mysocket1;
-    mysocket1 = socket(AF_INET, SOCK_STREAM, 0);
-    if (mysocket1 == -1) {
-        printf("socket() call failed\n");
-    }
+	int port = atoi(argv[1]);
+	int server_socket, client_socket, client_len, bytes_received, passcode, user_passcode, read_size;
+	struct sockaddr_in server_address, client_address;
+	char ucid[MAX_UCID_LEN];
+	char datetime[MAX_DATETIME_LEN];
+	char file_contents[MAX_FILE_LEN];
+	time_t now;
+	struct tm *timeinfo;
+	FILE *file;
 
-    int status;
-    status = bind(mysocket1, (struct sockaddr *)
-            &address, sizeof(struct sockaddr_in));
-    if (status == -1) {
-        printf("bind() call failed\n");
-    }
+	// Create server socket
+	server_socket = socket(AF_INET, SOCK_STREAM, 0);
+	if (server_socket < 0)
+	{
+		printf("Error creating server socket\n");
+		exit(EXIT_FAILURE);
+	}
 
-    status = listen(mysocket1, 5);
-    if (status == -1) {
-        printf("listen() call failed\n");
-    }
+	// Bind server socket to a specific port
+	memset(&server_address, 0, sizeof(server_address));
+	server_address.sin_family = AF_INET;
+	server_address.sin_addr.s_addr = htonl(INADDR_ANY);
+	server_address.sin_port = htons(port);
+	if (bind(server_socket, (struct sockaddr *)&server_address, sizeof(server_address)) < 0)
+	{
+		printf("Error binding server socket\n");
+		exit(EXIT_FAILURE);
+	}
 
-    int mysocket2;
-    mysocket2 = accept(mysocket1, NULL, NULL);
-    if (mysocket2 == -1) {
-        printf("accept() call failed\n");
-    }
+	// Listen for incoming connections
+	if (listen(server_socket, MAX_CLIENTS) < 0)
+	{
+		printf("Error listening for incoming connections\n");
+		exit(EXIT_FAILURE);
+	}
 
-    int count;
-    char rcv_message[100];
-    count = recv(mysocket2, rcv_message, 100, 0);
-    if (count == -1) {
-        printf("recv() call failed\n");
-    }
-    printf("%s\n", rcv_message);
+	while (1)
+	{
+		// Accept a new connection
+		client_len = sizeof(client_address);
+		client_socket = accept(server_socket, (struct sockaddr *)&client_address, (socklen_t *)&client_len);
+		if (client_socket < 0)
+		{
+			printf("Error accepting a new connection\n");
+			continue;
+		}
 
-    close(mysocket2);
-    close(mysocket1);
+		// Receive user's UCID from the client
+		bytes_received = recv(client_socket, ucid, MAX_UCID_LEN, 0);
+		if (bytes_received < 0)
+		{
+			printf("Error receiving user's UCID\n");
+			close(client_socket);
+			continue;
+		}
+		printf("Received UCID: %d\n", atoi(ucid));
+
+		// Send current day and time to the connected client
+		time(&now);
+		timeinfo = localtime(&now);
+		strftime(datetime, MAX_DATETIME_LEN, "%F %T", timeinfo);
+		datetime[MAX_DATETIME_LEN] = '\0';
+		if (send(client_socket, datetime, MAX_DATETIME_LEN, 0) < 0)
+		{
+			printf("Error sending current day and time\n");
+			close(client_socket);
+		}
+		printf("Sent current day and time: %s\n", datetime);
+
+		// Receive user's passcode from the client
+		bytes_received = recv(client_socket, &user_passcode, 4, 0);
+		if (bytes_received < 0)
+		{
+			printf("Error receiving user's passcode\n");
+			close(client_socket);
+			continue;
+		}
+
+		// Calculate passcode
+		passcode = (int)timeinfo->tm_sec + atoi(ucid) % 10000;
+		printf("Generated passcode: %d\n", passcode);
+
+		// Compare received password
+		if (user_passcode != passcode)
+		{
+			printf("Received mismatched passcode: %d\n", user_passcode);
+			close(client_socket);
+			continue;
+		}
+		printf("Received passcode: %d\n", user_passcode);
+
+		// Open the file "data.txt" from the run directory
+		file = fopen("data.txt", "r");
+		if (file == NULL)
+		{
+			printf("Error opening data.txt");
+			close(client_socket);
+			continue;
+		}
+
+		// Read the file contents into the file_contents buffer
+		memset(file_contents, 0, MAX_FILE_LEN);
+		if ((read_size = fread(file_contents, sizeof(char), MAX_FILE_LEN, file)) < 0)
+		{
+			printf("Error reading data.txt\n");
+			fclose(file);
+			close(client_socket);
+			continue;
+		}
+		file_contents[read_size] = '\0';
+
+		// Send the data to the client
+		if (send(client_socket, file_contents, read_size + 1, 0) < 0)
+		{
+			printf("Error sending data.txt\n");
+			continue;
+		}
+		printf("Sent data.txt: %s\n", file_contents);
+
+		// Close the file
+		fclose(file);
+	}
 }
