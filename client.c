dbd87926a3f35cd8bf1933b757be5f0fc73dcdb2
Joey Van Lierop <joey.vanlierop@me.com>
2023-01-29 14:36:06 -0700

    Add makefile and fix printfs

--- client.c
+++ client.c
@@ -7,26 +7,32 @@
 #include <sys/socket.h>
 
 int main() {
+    printf("This is the client\n");
+
     struct sockaddr_in address;
     memset(&address, 0, sizeof(address));
     address.sin_family = AF_INET;
     address.sin_port = htons(2000);
     address.sin_addr.s_addr = inet_addr("127.0.0.1");
+
     int mysocket1;
     mysocket1 = socket(AF_INET, SOCK_STREAM, 0);
     if (mysocket1 == -1) {
-        printf("socket() call failed");
+        printf("socket() call failed\n");
     }
+
     int status;
     status = connect(mysocket1, (struct sockaddr *) &address, sizeof(struct sockaddr_in));
     if (status == -1) {
-        printf("connect() call failed");
+        printf("connect() call failed\n");
     }
+
     int count;
     char snd_message[100] = {"hello"};
     count = send(mysocket1, snd_message, 5, 0);
     if (count == -1) {
-        printf("send() call failed.");
+        printf("send() call failed\n");
     }
+
     close(mysocket1);
 }
--- server.c
+++ server.c
@@ -5,39 +5,46 @@
 #include <string.h>
 
 int main() {
+    printf("This is the server\n");
+
     struct sockaddr_in address;
     memset(&address, 0, sizeof(address));
     address.sin_family = AF_INET;
     address.sin_port = htons(2000);
     address.sin_addr.s_addr = htonl(INADDR_ANY);
+
     int mysocket1;
     mysocket1 = socket(AF_INET, SOCK_STREAM, 0);
     if (mysocket1 == -1) {
-        printf("socket() call failed");
+        printf("socket() call failed\n");
     }
 
     int status;
     status = bind(mysocket1, (struct sockaddr *)
             &address, sizeof(struct sockaddr_in));
     if (status == -1) {
-        printf("bind() call failed");
+        printf("bind() call failed\n");
     }
+
     status = listen(mysocket1, 5);
     if (status == -1) {
-        printf("listen() call failed");
+        printf("listen() call failed\n");
     }
+
     int mysocket2;
     mysocket2 = accept(mysocket1, NULL, NULL);
     if (mysocket2 == -1) {
-        printf("accept() call failed");
+        printf("accept() call failed\n");
     }
+
     int count;
     char rcv_message[100];
     count = recv(mysocket2, rcv_message, 100, 0);
     if (count == -1) {
-        printf("recv() call failed.");
+        printf("recv() call failed\n");
     }
-    printf("%s", rcv_message);
+    printf("%s\n", rcv_message);
+
     close(mysocket2);
     close(mysocket1);
 }
